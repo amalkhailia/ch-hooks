507160be005650f567b12cba2c5a074542209cc7
amalkhailia <amalkhailia1995@gmail.com>
2021-02-13 16:33:00 +0100

    firstcommit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h3>movie_app session december!</h3>
+    </footer>
+  );
+};
+
+export default Footer;
